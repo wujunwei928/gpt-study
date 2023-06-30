e653a01d6614b39660f9e203b08c233093665526
wujunwei <1399952803@qq.com>
2023-06-30 23:58:02 +0800

    update

--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def bot(history):
 
 
 with gr.Blocks() as demo:
-    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=750)
+    chatbot = gr.Chatbot([], elem_id="chatbot").style(height=560)
 
     with gr.Row():
-        with gr.Column(scale=0.85):
+        with gr.Column(scale=85):
             txt = gr.Textbox(
                 show_label=False,
                 placeholder="Enter text and press enter, or upload an image",
             ).style(container=False)
-        with gr.Column(scale=0.15, min_width=0):
+        with gr.Column(scale=15, min_width=0):
             btn = gr.UploadButton("📁", file_types=["image", "video", "audio"])
 
     txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
